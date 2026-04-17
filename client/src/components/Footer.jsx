5c283e5d85daf1ff47e9ee4ae3ebe3443212c767
Gyaneshwar Singh <gyaneshwarsingh8986@gmail.com>
2026-04-17 20:54:32 +0530

    Update

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -36,7 +36,7 @@ const Footer = () => {
         </div>
     </div>
     <p className="pt-4 text-center text-xs md:text-sm pb-5">
-        Copyright 2025 © QuickAI All Right Reserved.
+        Copyright 2026 © QuickAI All Right Reserved.
     </p>
 </footer>
   )
